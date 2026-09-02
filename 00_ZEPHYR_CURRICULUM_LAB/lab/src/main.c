ab44af17c637fb5837c603fb75db4322a49a52b9
Jaeyoun You <jyounnim@gmail.com>
2026-09-02 13:17:05 +0900

    Modified the code to also output the core information(M4, M55).

--- 00_ZEPHYR_CURRICULUM_LAB/lab/src/main.c
+++ 00_ZEPHYR_CURRICULUM_LAB/lab/src/main.c
@@ -4,7 +4,7 @@
 
 int main(void) {
     while (1) {
-        printk("Hello, SR110! (Zephyr)\n");
+        printk("Hello, SR110! (Zephyr %s) \n", CONFIG_BOARD_TARGET);
         k_sleep(K_SECONDS(1));
     }
     return 0;
